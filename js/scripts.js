787b3e59e0ac66af97562810445900bad68251b1
Mantas <mantasadomav@gmail.com>
2017-04-04 02:47:55 +0300

    daily edit 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function(ev) {
 
 //-----functions---
-	var getColors = function() {
+	var getColors = function(colors) {
 		
 		var canvas2 = document.createElement('canvas');
 		var ctx2 = canvas2.getContext('2d');
@@ -9,44 +9,55 @@ window.addEventListener('load', function(ev) {
 		canvas2.height = spriteIMG.height;
 		ctx2.drawImage(spriteIMG, 0, 0);
 		
-		var imageData;
-		var id = spriteIMG.width / 32 * spriteIMG.height / 32;
-		alert(id);
+		//var id = spriteIMG.width / 32 * spriteIMG.height / 32;
+		var id = 32;
+		var x = 0;
+		var y = 0;
 		
-		for (var i = 0; i < 1000; i++)
+		for (var i = 0; i < id; i++)
 		{
-			//imageData = ctx.getImageData(x*32, y*32, 32, 32).data;
+			//x = i % spriteIMG.width / 32;
+			//y = i / spriteIMG.width / 32;
 			
+			imageData = ctx2.getImageData(x*32, y*32, 32, 32);
+			var data = imageData.data;
+			colors[i] = averageColor(data);
 			
+			setTimeout(function(i)
+			{
+				document.body.style.background = 'rgb('+colors[i].r+','+colors[i].g+','+colors[i].b+')';
+				
+			},400*i, i);
+			
+			x++;
 		}
 		
 	};
 	
 	
-	averageColor = function() {
-		  var imageData = ctx.getImageData(0, 0, width, height);
-  var data = imageData.data;
-  var r = 0;
-  var g = 0;
-  var b = 0;
+	var averageColor = function(data) {
 
-  for (var i = 0, l = data.length; i < l; i += 4) {
-    r += data[i];
-    g += data[i+1];
-    b += data[i+2];
-  }
+		var r = 0;
+		var g = 0;
+		var b = 0;
 
-  r = Math.floor(r / (data.length / 4));
-  g = Math.floor(g / (data.length / 4));
-  b = Math.floor(b / (data.length / 4));
+		for (var i = 0, l = data.length; i < l; i += 4) {
+			r += data[i];
+			g += data[i+1];
+			b += data[i+2];
+		}
 
-  return { r: r, g: g, b: b };
+		r = Math.floor(r / (data.length / 4));
+		g = Math.floor(g / (data.length / 4));
+		b = Math.floor(b / (data.length / 4));
+
+		return { r: r, g: g, b: b };
 	};
 //-----functions---
 
 
 //-----code--------
-	var colors;
+	var colors = [0];
 
 	var canvas = document.getElementById("myCanvas");
 	var ctx = canvas.getContext("2d");
@@ -59,7 +70,7 @@ window.addEventListener('load', function(ev) {
 
 
 //-----listeners---
-	startBtn.addEventListener('click', getColors);
+	startBtn.addEventListener('click', function(){getColors(colors);});
 //-----listeners---
 	
 });
