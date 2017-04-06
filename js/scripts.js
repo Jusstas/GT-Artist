311fb95bcd2bdd5abff5c3082e186448140ea514
Mantas <mantasadomav@gmail.com>
2017-04-06 19:28:09 +0300

    sorted code blocks

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,51 @@
 window.addEventListener('load', function(ev) {
 
-//-----functions---
-	var getColors = function(items) {
-		
-		var canvas2 = document.createElement('canvas');
-		var ctx2 = canvas2.getContext('2d');
+//-----code--------
+	var items = [];
+	
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	
+	var canvas2 = document.createElement('canvas');
+	var ctx2 = canvas2.getContext('2d');
+	
+	var spriteIMG = new Image();
+	spriteIMG.onload = function() {
 		canvas2.width = spriteIMG.width;
 		canvas2.height = spriteIMG.height;
 		ctx2.drawImage(spriteIMG, 0, 0);
+	};
+	spriteIMG.src = '../res/ItemSprites.png';
+	
+	var fileInput = document.getElementById("fileUpload");
+	var fileLabel = fileInput.nextElementSibling;
+	
+	var startBtn = document.getElementById("startBtn");
+//-----code--------
+
+
+//-----listeners---
+	fileInput.addEventListener('change', function()
+	{
+		var fileName = '';
+		
+		fileName = this.target.value.split('\\').pop();
+
+		if(fileName)
+			fileLabel.innerHTML = fileName;
+	});
+
+
+	startBtn.addEventListener('click', function()
+	{
+		this.disabled = true;
+		getColors(items);
+	});
+//-----listeners---
+
+
+//-----functions---
+	function getColors(items) {
 		
 		var div0 = document.getElementById("div0");
 		var dataString = ' ';
@@ -22,8 +60,7 @@ window.addEventListener('load', function(ev) {
 		
 		for (var i = 0; i < id; i++)
 		{
-			imageData = ctx2.getImageData(x*32, y*32, 32, 32);
-			var data = imageData.data;
+			var data = ctx2.getImageData(x*32, y*32, 32, 32).data;
 			
 			var colorObj = averageColor(data);
 			if (colorObj.pix > 0)
@@ -43,16 +80,14 @@ window.addEventListener('load', function(ev) {
 			}
 		}
 		div0.innerHTML = dataString;
-		alert('Done');
-	};
+	}
 	
 	
-	var averageColor = function(data) {
+	function averageColor(data) {
 
 		var r = 0;
 		var g = 0;
 		var b = 0;
-		
 		var pix = 0;
 
 		for (var i = 0, l = data.length; i < l; i += 4) {
@@ -72,44 +107,8 @@ window.addEventListener('load', function(ev) {
 		b = Math.floor(b / pix);
 
 		return { r: r, g: g, b: b, pix: pix };
-	};
+	}
 //-----functions---
-
-
-//-----code--------
-	var items = [];
-	
-	var spriteIMG = new Image();
-	spriteIMG.src = '../res/ItemSprites.png';
-
-	var canvas = document.getElementById("myCanvas");
-	var ctx = canvas.getContext("2d");
-	
-	var fileInput = document.getElementById("fileUpload");
-	var fileLabel = fileInput.nextElementSibling;
-	
-	var startBtn = document.getElementById("startBtn");
-//-----code--------
-
-
-//-----listeners---
-	fileInput.addEventListener( 'change', function(e)
-	{
-		var fileName = '';
-		
-		fileName = e.target.value.split( '\\' ).pop();
-
-		if(fileName)
-			fileLabel.innerHTML = fileName;
-	});
-
-
-	startBtn.addEventListener('click', function()
-	{
-		this.disabled = true;
-		getColors(items);
-	});
-//-----listeners---
 	
 });
 
